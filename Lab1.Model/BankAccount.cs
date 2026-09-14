17301fe186f16de1cb0fb506c7cc1d331c378005
Паймышев Никита <nikita.paymyshev@gmail.com>
2026-09-15 01:28:08 +0700

    feat: Добавлена модель BankAccount и бизнес-логика

---
+++ Lab1.Model/BankAccount.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace laba1.Models
+{
+    public class BankAccount
+    {
+        
+        public string AccountNumber { get; set; }
+        public string AccountOwner { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsActive { get; set; } = true; 
+
+       
+        public BankAccount()
+        {
+        }
+
+        
+        public BankAccount(string accountNumber, string accountOwner, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            AccountOwner = accountOwner;
+            Balance = initialBalance;
+            IsActive = true;
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount > 0 && IsActive)
+            {
+                Balance += amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance && IsActive)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool EditOwner(string newOwner)
+        {
+            if (!string.IsNullOrWhiteSpace(newOwner))
+            {
+                AccountOwner = newOwner;
+                return true;
+            }
+            return false;
+        }
+
+        public bool FreezeAccount()
+        {
+            if (IsActive)
+            {
+                IsActive = false;
+                return true;
+            }
+            return false;
+        }
+
+        public bool UnfreezeAccount()
+        {
+            if (!IsActive)
+            {
+                IsActive = true;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Transfer(BankAccount targetAccount, decimal amount)
+        {
+            if (targetAccount != null && Withdraw(amount))
+            {
+                targetAccount.Deposit(amount);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Lab1.Model/Logic.cs
@@ -0,0 +1,56 @@
+﻿using laba1.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.Model
+{
+    internal class Logic
+    {
+        public List<BankAccount> Accounts { get; } = new List<BankAccount>();
+
+        public Logic()
+        {
+            
+            Accounts.Add(new BankAccount("40817810000000000001", "Алексей Смирнов", 45000m));
+            Accounts.Add(new BankAccount("40817810000000000002", "Ирина Кузнецова", 128000m));
+            Accounts.Add(new BankAccount("40817810000000000003", "Дмитрий Соколов", 3500m));
+            Accounts.Add(new BankAccount("40817810000000000004", "Ольга Попова", 92000m));
+
+        }
+
+        public void AddAccount(BankAccount account) => Accounts.Add(account);
+
+        public BankAccount? GetAccount(string accountNumber) => Accounts.Find(a => a.AccountNumber == accountNumber);
+
+        public List<BankAccount> GetActiveAccount() => Accounts.Where(x => x.IsActive).ToList();
+
+        public void FreezeAccountNumber(string accountNumber)
+        {
+            var account = GetAccount(accountNumber);
+            if (account != null)
+            {
+                account.FreezeAccount();
+            }
+        }
+
+        public void UnfreezeAccountNumber(string accountNumber)
+        {
+            var account = GetAccount(accountNumber);
+            if (account != null)
+            {
+                account.UnfreezeAccount();
+            }
+        }
+
+        public void EditAccountOwner(string accountNumber, string newOwner)
+        {
+            var account = GetAccount(accountNumber);
+            if (account != null)
+            {
+                account.EditOwner(newOwner);
+            }
+        }
+    }
+}
