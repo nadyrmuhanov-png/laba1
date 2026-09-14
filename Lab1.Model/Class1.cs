3447ee560ac1a610ddb5af73fb9a959a744754c0
Java <nadyrmuhanov@gmail.com>
2026-09-14 20:52:20 +0700

    Initial project structure: Model, Console, WinForms

---
+++ Lab1.Model/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab1.Model;
+
+public class Class1
+{
+
+}
---
+++ Lab1.WinForms/Form1.cs
@@ -0,0 +1,9 @@
+namespace Lab1.WinForms;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
