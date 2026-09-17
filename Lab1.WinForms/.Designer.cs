fe0adbfa387b5ba8e0c96c20bcdca1d0853c1cb5
Java <nadyrmuhanov@gmail.com>
2026-09-17 18:06:52 +0700

    че добавил

--- Lab1.WinForms/.Designer.cs
+++ Lab1.WinForms/.Designer.cs
@@ -1,38 +1,84 @@
-﻿namespace Lab1.WinForms;
-
-partial class MainForm
+﻿namespace Lab1.WinForms
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class MainForm
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            txtAccountNumber = new TextBox();
+            txtOwnerName = new TextBox();
+            btnCreate = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Top;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(800, 300);
+            dataGridView1.TabIndex = 0;
+            //
+            // txtAccountNumber
+            //
+            txtAccountNumber.Location = new Point(20, 320);
+            txtAccountNumber.Name = "txtAccountNumber";
+            txtAccountNumber.Size = new Size(200, 23);
+            txtAccountNumber.TabIndex = 1;
+            txtAccountNumber.PlaceholderText = "Номер счёта";
+            //
+            // txtOwnerName
+            //
+            txtOwnerName.Location = new Point(240, 320);
+            txtOwnerName.Name = "txtOwnerName";
+            txtOwnerName.Size = new Size(200, 23);
+            txtOwnerName.TabIndex = 2;
+            txtOwnerName.PlaceholderText = "ФИО владельца";
+            //
+            // btnCreate
+            //
+            btnCreate.Location = new Point(460, 320);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(100, 23);
+            btnCreate.TabIndex = 3;
+            btnCreate.Text = "Создать";
+            btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            components = new System.ComponentModel.Container();
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCreate);
+            Controls.Add(txtOwnerName);
+            Controls.Add(txtAccountNumber);
+            Controls.Add(dataGridView1);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox txtAccountNumber;
+        private TextBox txtOwnerName;
+        private Button btnCreate;
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        components = new System.ComponentModel.Container();
-        AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
-        Text = "Form1";
-    }
-
-    #endregion
 }
--- Lab1.WinForms/MainForm.cs
+++ Lab1.WinForms/MainForm.cs
@@ -1,9 +1,49 @@
-namespace Lab1.WinForms;
+using laba1.Models;
 
-public partial class MainForm : Form
+namespace Lab1.WinForms
 {
-    public MainForm()
+    public partial class MainForm : Form
     {
-        InitializeComponent();
+        private Logic logic = new Logic();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Load += MainForm_Load;
+            btnCreate.Click += btnCreate_Click;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void RefreshGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = logic.GetActiveAccount();
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата!"); // временная проверка
+
+            string accountNumber = txtAccountNumber.Text;
+            string ownerName = txtOwnerName.Text;
+
+            if (string.IsNullOrWhiteSpace(accountNumber) || string.IsNullOrWhiteSpace(ownerName))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+
+            var newAccount = new BankAccount(accountNumber, ownerName, 0m);
+            logic.AddAccount(newAccount);
+
+            RefreshGrid();
+
+            txtAccountNumber.Clear();
+            txtOwnerName.Clear();
+        }
+
     }
 }
